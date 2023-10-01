ed6f9b405119fe44abda487032b6cff70d9e6cb1
KristinaBu <krisdinabui@gmail.com>
2023-10-01 23:50:56 +0300

    lab2 without report

---
+++ Lab_2/lab_python_oop/color.py
@@ -0,0 +1,45 @@
+
+class Color:
+    """
+    Класс «Цвет фигуры»
+    """
+    def __init__(self):
+        self._color = None
+    @property
+    def colorproperty(self):
+        """
+        Get-аксессор
+        """
+        return self._color
+    @colorproperty.setter
+    def colorproperty(self, value):
+        """
+        Set-аксессор
+        """
+        self._color = value
+
+class FigureColor1:
+    """
+    Класс «Цвет фигуры»
+    """
+    def init(self):
+        self._color = None
+    def get_color(self):
+        """
+        Get-аксессор
+        """
+        return self._color
+
+    def set_color(self, value):
+        """
+        Set-аксессор
+        """
+        self._color = value
+"""
+a = FigureColor()
+b = FigureColor1()
+a.colorproperty = 2
+b.set_color(2)
+print(a.colorproperty, b.get_color())
+"""
+
---
+++ Lab_2/main.py
@@ -0,0 +1,47 @@
+from lab_python_oop.rectangle import Rectangle
+from lab_python_oop.circle import Circle
+from lab_python_oop.square import Square
+import pyfiglet
+import matplotlib.pyplot as plt
+
+
+def main():
+    r1 = Rectangle("синего", 3, 2)
+    r2 = Rectangle("зеленого", 4, 3)
+    r3 = Rectangle("красного", 5, 4)
+    c1 = Circle("синего", 5)
+    c2 = Circle("зеленого", 6)
+    c3 = Circle("красного", 7)
+    s1 = Square("синего", 5)
+    s2 = Square("зеленого", 6)
+    s3 = Square("красного", 7)
+
+
+    #Первый способ
+    result = pyfiglet.figlet_format("Laba - 2", font="3-d")
+    print(result)
+    #Второй способ
+    f = pyfiglet.Figlet(font='slant')
+    ascii_art = f.renderText('programmers rule the world')
+    print(ascii_art)
+
+    #График
+    squares = [r1.square(),r2.square(),r3.square(),
+               c1.square(),c2.square(),c3.square(),
+               s1.square(),s2.square(),s3.square()]
+    for sq in squares: print(sq)
+
+
+    fig, ax = plt.subplots()
+    #заголовки
+    ax.set_title("Squares", fontsize=14)
+    x_value = [r1.FIGURE_TYPE[:2] + '1', r2.FIGURE_TYPE[:2] + '2', r3.FIGURE_TYPE[:2] + '3',
+               c1.FIGURE_TYPE[:2] + '1', c2.FIGURE_TYPE[:2] + '2', c3.FIGURE_TYPE[:2] + '3',
+               s1.FIGURE_TYPE[:2] + '1', s2.FIGURE_TYPE[:2] + '2', s3.FIGURE_TYPE[:2] + '3']
+
+    plt.plot(x_value, sorted(squares))
+    plt.xticks(rotation=90)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
